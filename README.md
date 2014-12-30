9fadc00801bc06a42ceb58889efe1fd310b66b08
zhangyaochun <zhangyaochun@wandoujia.com>
2014-12-30 11:54:10 +0800

    init

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 wd-deploy
-=========
+===========
 
-deploy tools
+命令行工具，来自动执行git操作：
+
+```javascript
+remote			远程的git库，一般就是wandoulabs上的
+silent    		Do not echo program output to console
+branch			分支支持，默认是master
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+require('shelljs/global');
+
+var rimraf = require('rimraf');
+
+module.exports = function (opts) {
+    if (!which('get')) {
+      throw new Error('You shoule install git first');
+      exit(1);
+    }  
+
+    var config = {
+      silent: true,
+      deepSilent: false,
+      message: 'commit auto by wd-deploy on ';
+      branch: 'master'
+    };
+
+    //
+    var cwd = process.cwd();
+
+    var dirpath = cwd;
+    //关于创建目录的策略
+    cd(dirpath);
+
+    //已经拉取了的
+    if (exec('git status ' + dirpath, {silent: config.silent}).code !== 0) {
+        cd('/..');
+        rimraf(dirpath);
+
+        if (exec('git clone ' + config.remote + ' ' + dirpath).code !== 0) {
+            throw new Error('Git clone failed');
+            exit(1);
+        }
+    }
+
+
+    cd(dirpath);
+
+    //没有clone过的就checkout 
+    exec('git checkout ' + config.branch, {silent: config.silent});
+
+    exec('git reset --hard HEAD^', {silent: config.silent});
+
+    exec('git pull', {silent: config.silent});
+
+
+    //git add 
+    exec('git add -A', {silent: config.silent});
+
+    //git status
+    exec('git status', {silent: config.deepSilent});
+
+    //git commit 
+    //add time or other msg your like
+    exec('git commit -m "' + config.message + (new Date().getTime()) + '"', {silent: config.deepSilent});
+
+    //git push
+    exec('git push', {silent: config.deepSilent})
+};
